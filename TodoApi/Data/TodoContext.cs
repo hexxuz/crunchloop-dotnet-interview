f8cf2197e2774c89d5c343b6b7a39c778a774be0
hexxuz <gomezfed15@gmail.com>
2025-03-28 15:12:23 -0300

    [DB/BE] TodoItems entity implementation & DbContext improvement

--- TodoApi/Data/TodoContext.cs
+++ TodoApi/Data/TodoContext.cs
@@ -1,10 +1,19 @@
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
+using TodoApi.Interfaces;
+using TodoApi.Models.Items;
+using TodoApi.Models.Lists;
 
-public class TodoContext : DbContext
+public class TodoContext : DbContext, IDbContext
 {
     public TodoContext(DbContextOptions<TodoContext> options)
         : base(options) { }
 
-    public DbSet<TodoList> TodoList { get; set; } = default!;
+
+    #region Todo Lists
+    public DbSet<TodoList> TodoList => Set<TodoList>();
+    #endregion
+
+    #region Todo Items
+    public DbSet<TodoItem> TodoItem => Set<TodoItem>();
+    #endregion
 }
---
+++ TodoApi/Interfaces/IDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models.Items;
+using TodoApi.Models.Lists;
+
+namespace TodoApi.Interfaces
+{
+    public interface IDbContext
+    {
+        #region Todo Lists
+        DbSet<TodoList> TodoList { get; }
+        #endregion
+
+        #region Todo Items
+        DbSet<TodoItem> TodoItem { get; }
+        #endregion
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
