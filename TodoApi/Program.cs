0a65e8b4c927bd00f24c9da8ac3096509a3c0ce3
hexxuz <gomezfed15@gmail.com>
2025-03-31 03:31:41 -0300

    [BE] CorsPolicy fix

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -16,12 +16,24 @@ builder
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    })
+    .Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "FrontEndUI", policy =>
+        {
+            policy.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+        });
     });
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("FrontEndUI");
+}
 
 app.UseAuthorization();
 app.MapControllers();
